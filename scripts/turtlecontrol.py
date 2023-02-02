cbae5e106b6f82f30c04a32ecdf37cc8b836ae69
alexar5035 <alexar5035@gmail.com>
2023-02-02 21:27:03 +0000

    publisher code

--- scripts/turtlecontrol.py
+++ scripts/turtlecontrol.py
@@ -12,33 +12,36 @@ from turtlesim.msg import Pose
 # message we created
 from robotics_lab1.msg import Turtlecontrol
 
-# for radians to degrees conversions
-import math
-
 pos_msg = Turtlecontrol();
-
+# declare variables for the proportional controller equation
+kp = 0
+xd = 0
+xt = 0
 # recieves position information
-def pose_callback(pos_callback):
-	# convert x and y to cm
-	pos_msg.x = data.x * 100
-	pos_msg.y = data.y * 100
-	
-	
-def user_control(Turtlecontrol_callback):
-	# convert x and y to cm
-	pos_msg.xd = data.x * 100
-	pos_msg.kp = data.y * 100
+def pose_callback(data):
+	global pos_msg
+	global xt
+	# convert x 
+	pos_msg.xt = data.x #TURTLECONTROL OBJECT HAS NO ATTRIBUTE "X"
+		
+def user_control_callback(data):
+	global pos_msg
+	global xd
+	global kp
+	# convert xd and kp
+	pos_msg.xd = data.x 
+	pos_msg.kp = data.y 
 	
 
 if __name__ == '__main__':
 	# initialize the node
-	rospy.init_node('pos_converter', anonymous = True)
+	rospy.init_node('turtlecontrol.py', anonymous = True)
 	
 	# add a subscriber to it to read the position information
 	rospy.Subscriber('/turtle1/pose', Pose, pose_callback)
 	
 	# add a subscriber to it to read the user control information
-	rospy.Subscriber('/turtle1/control_params', Turtlecontrol, Turtlecontrol_callback)
+	rospy.Subscriber('/turtle1/control_params', Turtlecontrol, user_control_callback)
 	
 	# add a publisher
 	pos_pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size = 10)
@@ -46,23 +49,17 @@ if __name__ == '__main__':
 	# 10Hz freq
 	loop_rate = rospy.Rate(10)
 	
-	while not rospy.is_shutdown():
-		pos_pub.publish(pos_msg)
-		loop_rate.sleep()
 	# declare a variable of type Twist for sending control commands
 	vel_cmd = Twist()
 	
-	# initialize the node
-	rospy.init_node('turtlecontrol.py', anonymous = True)
-
-	# set up loop that runs at a 10Hz frequency
 	while not rospy.is_shutdown():
+		# pos_pub.publish(pos_msg)
 		# set the linear (forward/backward) velocity command to 0.5 m/s
-		vel_cmd.linear.x = 0.5
+		vel_cmd.linear.x = kp * (xd - xt) # proportional control equation
 		# set the angular (heading) velocity command to 0.5 radians/s
-		vel_cmd.angular.z = 0.5
+		vel_cmd.angular.z = kp * (xd - xt) # proportional control equation
 		# publish the command to the defined topic
-		cmd_pub.publish(vel_cmd)
+		pos_pub.publish(vel_cmd)
 		# wait for 0.1 seconds until the next loop and repeat
 		loop_rate.sleep()
 	
